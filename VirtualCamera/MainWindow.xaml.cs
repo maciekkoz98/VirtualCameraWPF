6b5aa777c52abdfaa4f38257acf989dfacf61d04
Maciej Kozłowski <maciekkoz98@gmail.com>
2020-03-22 21:35:58 +0100

    json configuration, scene drawing (needs numerous fixes)

--- VirtualCamera/MainWindow.xaml.cs
+++ VirtualCamera/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using VirtualCamera.Utils;
 
 namespace VirtualCamera
 {
@@ -28,30 +31,89 @@ namespace VirtualCamera
 
         private void LoadSceneFromFile(object sender, RoutedEventArgs e)
         {
-            //load text file and create the scene
             sceneCanvas.Background = Brushes.Black;
-            Line line = new Line
+            string file = "";
+            try
             {
-                X1 = 20,
-                X2 = 20,
-                Y1 = 99,
-                Y2 = 50
-            };
-            line.Stroke = Brushes.White;
-            line.StrokeThickness = 2;
-            sceneCanvas.Children.Add(line);
+                using (StreamReader sr = new StreamReader("scene.json"))
+                {
+                    file += sr.ReadToEnd();
+                }
+
+            }
+            catch (IOException)
+            {
+                MessageBoxResult result = MessageBox.Show("Brakuje pliku konfiguracyjnego", "VirtualCamera", MessageBoxButton.OK, MessageBoxImage.Error);
+                if (result == MessageBoxResult.OK)
+                {
+                    Environment.Exit(1);
+                }
+            }
+            Scene fileScene = JsonConvert.DeserializeObject<Scene>(file);
+           // fileScene.MakeCameraPosZeroPoint();
+            fileScene.From3Dto2D();
+            List<double[,]> pointsList = fileScene.CreatePointsList();
+            foreach (double[,] points in pointsList)
+            {
+                DrawRectangle(points);
+            }
+        }
+
+        private void DrawRectangle(double[,] points)
+        {
+            for (int i = 0; i < points.GetLength(0); i++)
+            {
+                Line line;
+                if (i == points.GetLength(0) - 1)
+                {
+                    line = new Line
+                    {
+                        X1 = 400 + points[i, 0],
+                        Y1 = 310 + points[i, 1],
+                        X2 = 400 + points[0, 0],
+                        Y2 = 310 + points[0, 1],
+                        Stroke = Brushes.Green,
+                        StrokeThickness = 2
+                    };
+
+                }
+                else
+                {
+                    line = new Line
+                    {
+                        X1 = 400 + points[i, 0],
+                        Y1 = 310 + points[i, 1],
+                        X2 = 400 + points[i + 1, 0],
+                        Y2 = 310 + points[i + 1, 1],
+                        //Stroke = Brushes.White,
+                        StrokeThickness = 2
+                    };
+                }
+                if (i % 4 == 0)
+                {
+                    line.Stroke = Brushes.Purple;
+                }
+                else if (i % 4 == 1)
+                {
+                    line.Stroke = Brushes.Yellow;
+                }
+                else if (i % 4 == 2)
+                {
+                    line.Stroke = Brushes.Orange;
+                }
+                sceneCanvas.Children.Add(line);
+            }
+
         }
 
         private void KeyboardKeyPressed(object sender, KeyEventArgs e)
         {
-            Trace.WriteLine("Przed if");
             if (e.Key == Key.D)
             {
-                Trace.WriteLine("w IF");
                 Line child = (Line)sceneCanvas.Children[0];
                 child.X1 += 50;
-                //sceneCanvas.Children.Remove(child);
-            } else if (e.Key == Key.A)
+            }
+            else if (e.Key == Key.A)
             {
                 Line child = (Line)sceneCanvas.Children[0];
                 child.X1 -= 50;
---
+++ VirtualCamera/Utils/Scene.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualCamera.Utils
+{
+    class Scene
+    {
+        private readonly int distance = 500;
+
+        public int[] CameraPos { get; set; }
+        public List<List<double>> Cuboid1 { get; set; }
+        public List<List<double>> Cuboid2 { get; set; }
+        public List<List<double>> Cuboid3 { get; set; }
+        public List<List<double>> Cuboid4 { set; get; }
+
+        public void MakeCameraPosZeroPoint()
+        {
+            for (int i = 0; i < Cuboid1.Count; i++)
+            {
+                for (int j = 0; j < Cuboid1[i].Count; j++)
+                {
+                    Cuboid1[i][j] = Cuboid1[i][j] - CameraPos[j];
+                    Cuboid2[i][j] = Cuboid2[i][j] - CameraPos[j];
+                    Cuboid3[i][j] = Cuboid3[i][j] - CameraPos[j];
+                    Cuboid4[i][j] = Cuboid4[i][j] - CameraPos[j];
+                }
+            }
+        }
+
+        public void From3Dto2D()
+        {
+            for (int i = 0; i < Cuboid1.Count; i++)
+            {
+                for (int j = 0; j < Cuboid1[i].Count; j++)
+                {
+                    Cuboid1[i][j] = Cuboid1[i][j] * distance / (Cuboid1[i][2] + distance);
+                    Cuboid2[i][j] = Cuboid2[i][j] * distance / (Cuboid2[i][2] + distance);
+                    Cuboid3[i][j] = Cuboid3[i][j] * distance / (Cuboid3[i][2] + distance);
+                    Cuboid4[i][j] = Cuboid4[i][j] * distance / (Cuboid4[i][2] + distance);
+                }
+            }
+        }
+
+        public List<double[,]> CreatePointsList()
+        {
+            List<double[,]> pointsList = new List<double[,]>
+            {
+                //new double[4, 2] { { Cuboid1[1][0], Cuboid1[1][1] }, { Cuboid1[5][0], Cuboid1[5][1] }, { Cuboid1[6][0], Cuboid1[6][1] }, { Cuboid1[2][0], Cuboid1[2][1] } },
+                //new double[4, 2] { { Cuboid1[0][0], Cuboid1[0][1] }, { Cuboid1[4][0], Cuboid1[4][1] }, { Cuboid1[5][0], Cuboid1[5][1] }, { Cuboid1[1][0], Cuboid1[1][1] } },
+                //new double[4, 2] { { Cuboid1[3][0], Cuboid1[3][1] }, { Cuboid1[7][0], Cuboid1[7][1] }, { Cuboid1[6][0], Cuboid1[6][1] }, { Cuboid1[2][0], Cuboid1[2][1] } },
+                //new double[4, 2] { { Cuboid1[0][0], Cuboid1[0][1] }, { Cuboid1[4][0], Cuboid1[4][1] }, { Cuboid1[7][0], Cuboid1[7][1] }, { Cuboid1[3][0], Cuboid1[3][1] } },
+                new double[4, 2] { { Cuboid2[0][0], Cuboid2[0][1] }, { Cuboid2[1][0], Cuboid2[1][1] }, { Cuboid2[2][0], Cuboid2[2][1] }, { Cuboid2[3][0], Cuboid2[3][1] } },
+                new double[4, 2] { { Cuboid2[0][0], Cuboid2[0][1] }, { Cuboid2[4][0], Cuboid2[4][1] }, { Cuboid2[5][0], Cuboid2[5][1] }, { Cuboid2[1][0], Cuboid2[1][1] } },
+                new double[4, 2] { { Cuboid2[3][0], Cuboid2[3][1] }, { Cuboid2[7][0], Cuboid2[7][1] }, { Cuboid2[6][0], Cuboid2[6][1] }, { Cuboid2[2][0], Cuboid2[2][1] } },
+                new double[4, 2] { { Cuboid2[1][0], Cuboid2[1][1] }, { Cuboid2[5][0], Cuboid2[5][1] }, { Cuboid2[6][0], Cuboid2[6][1] }, { Cuboid2[2][0], Cuboid2[2][1] } },
+                //new double[4, 2] { { Cuboid3[0][0], Cuboid3[0][1] }, { Cuboid3[1][0], Cuboid3[1][1] }, { Cuboid3[2][0], Cuboid3[2][1] }, { Cuboid3[3][0], Cuboid3[3][1] } },
+                //new double[4, 2] { { Cuboid3[0][0], Cuboid3[0][1] }, { Cuboid3[4][0], Cuboid3[4][1] }, { Cuboid3[5][0], Cuboid3[5][1] }, { Cuboid3[1][0], Cuboid3[1][1] } },
+                //new double[4, 2] { { Cuboid3[3][0], Cuboid3[3][1] }, { Cuboid3[7][0], Cuboid3[7][1] }, { Cuboid3[6][0], Cuboid3[6][1] }, { Cuboid3[2][0], Cuboid3[2][1] } },
+                //new double[4, 2] { { Cuboid3[1][0], Cuboid3[1][1] }, { Cuboid3[5][0], Cuboid3[5][1] }, { Cuboid3[6][0], Cuboid3[6][1] }, { Cuboid3[2][0], Cuboid3[2][1] } },
+                //new double[4, 2] { { Cuboid4[0][0], Cuboid4[0][1] }, { Cuboid4[1][0], Cuboid4[1][1] }, { Cuboid4[2][0], Cuboid4[2][1] }, { Cuboid4[3][0], Cuboid4[3][1] } },
+                //new double[4, 2] { { Cuboid4[0][0], Cuboid4[0][1] }, { Cuboid4[4][0], Cuboid4[4][1] }, { Cuboid4[5][0], Cuboid4[5][1] }, { Cuboid4[1][0], Cuboid4[1][1] } },
+                //new double[4, 2] { { Cuboid4[3][0], Cuboid4[3][1] }, { Cuboid4[7][0], Cuboid4[7][1] }, { Cuboid4[6][0], Cuboid4[6][1] }, { Cuboid4[2][0], Cuboid4[2][1] } },
+                //new double[4, 2] { { Cuboid4[1][0], Cuboid4[1][1] }, { Cuboid4[5][0], Cuboid4[5][1] }, { Cuboid4[6][0], Cuboid4[6][1] }, { Cuboid4[2][0], Cuboid4[2][1] } }
+            };
+            return pointsList;
+        }
+    }
+}
